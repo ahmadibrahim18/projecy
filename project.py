0dee1dd8477c84f6ca5baa41368016a62f5d42f7
ahmadibrahimua18 <ahmadibrahimua18@gmail.com>
2024-07-15 23:10:23 +0300

    add welcome function this let the user to choose what he want

--- project.py
+++ project.py
@@ -1,25 +1,46 @@
+
+class main:
+    def welcome():
+        option1 = input("1.to go to the drivers menu:")
+        option2 = input("2.to go to the cities menu:")
+        option3 = input("3.to exit the system:")
+
+
+
 class menudrivers:
     
+    def __init_subclass__(cls):
+        pass
     def __init__(self,name,startcity) :
     
-        self.workerid = "id00"
-        self.name = name
-        self.startcity = startcity
+       self.workerid = "id00"
+       self.name = name
+       self.startcity = startcity
     
-    def view_drivers():
-        for i in driverlist:
-            print(driverlist[i],end="\n")
+    #function to view drivers
 
+
+# here is the add driver function
     def add_drivers():
+        
         driver_name = input("enter the driver name:")
         
         driver_startcity = input("enter the driver start city:")
         newdriver = (driver_name,driver_startcity)
         driverlist.append(newdriver)
+    
+    
+    def view_drivers():
+        for i in driverlist:
+            print(driverlist[i],end="\n")
         
 
 
-driver1 = menudrivers("max verstappen","akkar")
-driverlist=[]
+
+print(menudrivers.add_drivers())
+print(menudrivers.view_drivers())
+
+
+
 
 
