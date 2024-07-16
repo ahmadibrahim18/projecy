5a04d42f544c789620af0441cbbda2c0366d804b
ahmadibrahimua18 <ahmadibrahimua18@gmail.com>
2024-07-16 22:39:49 +0300

    created the cities class

--- project.py
+++ project.py
@@ -26,25 +26,33 @@ class menudrivers:
        self.name = name
        self.startcity = startcity
     
-    #function to view drivers
+   
 
 
 # here is the add driver function
     def add_drivers():
         result = True
+        #while loop to add another driver
         while result == True:
             driver_name = input("enter the driver name:")
         
             driver_startcity = input("enter the driver start city:")
-            for i in cities.cities:
-                if driver_startcity != driver_startcity:
-                    input = input("this city is not added , do you want to add it ? :")
-                    if input == "yes":
-                        cities.cities.append(driver_startcity)
+            # check if the added city by the user is already exist or not 
+            for i in cities.citylist:
+                if cities.citylist != driver_startcity:
+                    user_answer = input("this city is not added , do you want to add it ? :")
+                    if user_answer == "yes":
+                        # ask the user if he want to add the city to database
+                        cities.citylist.append(driver_startcity)
                     else:
                         print("the city will not be added")
+            # create an object for a new driver
             newdriver = (driver_name,driver_startcity)
+
+            #add the driver to driver list
             menudrivers.driverlist.append(newdriver)
+
+            # ask the user if he want to add another driver
             answer = input("do you want to add another driver ?")
             if answer == "yes" :
                 result = True
@@ -59,7 +67,7 @@ class menudrivers:
     def view_drivers():
         counter = 0
         for i in menudrivers.driverlist:
-
+            #print all the drivers in the list
             print(menudrivers.driverlist[counter],end="\n")
             counter+=1
         
@@ -71,7 +79,17 @@ print(menudrivers.view_drivers())
 
 
 class cities:
-    cities = []
+    
+    def __init__(self,name) :
+        self.name = name
+
+    beirut = ("beirut")
+    jbeil = ("jbeil")
+    akkar = ("akkar")
+    zahle =("zahle")
+
+    def add_city(cities city):
+
 
 
 
