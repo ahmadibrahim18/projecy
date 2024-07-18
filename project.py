9d9561cd1e036108ce14a609ea563b0d0dbdc817
ahmadibrahimua18 <ahmadibrahimua18@gmail.com>
2024-07-18 17:21:45 +0300

    corrected the options in menudrivers so i can go back to main menu

--- project.py
+++ project.py
@@ -2,208 +2,168 @@ import sys
 
 class main:
     def __init__(self):
-        self.menu_drivers = menudrivers()
-        self.cities = cities()
+        self.menu_drivers = menudrivers(self)
+        self.cities = cities(self)
     
     def welcome(self):
-        option1 = input("1.to go to the drivers menu:  2.to go to the cities menu:   3.to exit the system:")
+        option1 = input("1. to go to the drivers menu\n2. to go to the cities menu\n3. to exit the system: ")
         
-
         if option1 == "1":
-          self.menu_drivers.menudriveroption
-        elif option1 =="2":
-             self.cities.citiesoption
+            self.menu_drivers.menudriveroption()
+        elif option1 == "2":
+            self.cities.citiesoption()
         else:
             sys.exit()
 
-
-
-
-
-#"""""
 class menudrivers:
     driverlist = []
-    def __init__(self):
-        pass
+    def __init__(self, main_instance):
+        self.main_instance = main_instance
+   
 
-
-
-# here is the add driver function
     def add_drivers(self):
         result = True
-        #while loop to add another driver
-        while result == True:
-            driver_name = input("enter the driver name:")
-        
-            driver_startcity = input("enter the driver start city:")
-            # check if the added city by the user is already exist or not 
-            for i in cities.citylist:
-                
-                if cities.citylist != driver_startcity:
-                    
-                    user_answer = input("this city is not added , do you want to add it ? :")
-                    
-                    if user_answer == "yes":
-                        
-                        near_city = input("enter the nearest city :")
-                        # ask the user if he want to add the city to database
-                        
-                        cities.citylist.append(driver_startcity)
-                        
-                        return cities.addEdge(cities.city_list,driver_startcity,near_city)
-                    
-                    else:
-                        
-                        print("the city will not be added")
-            # create an object for a new driver
+        while result:
+            driver_name = input("Enter the driver name: ")
+            driver_startcity = input("Enter the driver start city: ")
             
-            newdriver = (driver_name,driver_startcity)
+            if driver_startcity not in cities.city_list:
+                user_answer = input("This city is not added, do you want to add it? (yes/no): ")
+                if user_answer.lower() == "yes":
+                    near_city = input("Enter the nearest city: ")
+                    cities.add_city(driver_startcity)
+                    cities.addEdge(cities.city_graph, driver_startcity, near_city)
+                else:
+                    print("The city will not be added")
+                    continue
 
-            #add the driver to driver list
+            newdriver = (driver_name, driver_startcity)
             menudrivers.driverlist.append(newdriver)
 
-            # ask the user if he want to add another driver
-            answer = input("do you want to add another driver ?")
-            if answer == "yes" :
+            answer = input("Do you want to add another driver? (yes/no): ")
+            if answer == "yes":
                 result = True
-                
             else:
-                result == False
-               # print(menudrivers.view_drivers())
-        
-
-    
+                main.welcome
+                result = False
+                
     
     def view_drivers(self):
-        if len(menudrivers.driverlist) == 0:
-            print("no drivers to show")
-        counter = 0
-        for i in menudrivers.driverlist:
-            #print all the drivers in the list
-            print(menudrivers.driverlist[counter],end="\n")
-            counter+=1
+        if len(menudrivers.driverlist):
+            print("No drivers to show")
+        else:
+            for driver in menudrivers.driverlist:
+                print(driver)
         
     def menudriveroption(self):
-        options = input("1. to view all drivers \n 2.to add driver \n 3.to go back to main menu")
+        options = input("1. to view all drivers\n2. to add driver\n3. to go back to main menu\n")
+        while True:
+            
 
-        if options == 1 :
-            self.add_drivers()
-        elif options ==2:
-            self.view_drivers()
-        elif options == 3:
-            sys.exit
-        else:
-            print("invalid input, Enter 1,2,3 .")
+            if options == "1":
+                self.view_drivers()
+            elif options == "2":
+                self.add_drivers()
+            elif options == "3":
+                self.main_instance.welcome()
+              
+            else:
+                print("Invalid input, Enter 1, 2, or 3.")
 
 class cities:
     city_list = []
     city_graph = {}
-    def __init__(self) :
-        pass
 
-    beirut = ("beirut")
-    jbeil = ("jbeil")
-    akkar = ("akkar")
-    zahle =("zahle")
-    saida = ("saida")
+    def __init__(self, main_instance):
+        self.main_instance = main_instance
+
+    @staticmethod
     def addEdge(adj, main_city, edgecity):
-
-        adj[main_city].append(edgecity)
-        adj[edgecity].append(main_city)
-
-    def citiesoption():
-        answer = input("1. To Show Cities \n 2.Print Neighboring Cities \n 3.Print Drivers delivering to City")
-
-        if answer == "1":
-            return cities.showcities()
-        elif answer == "2":
-            return cities.neighbor_cities()
-        elif answer =="3":
-            return cities.drivers_to_city()
+        if main_city in adj:
+            adj[main_city].append(edgecity)
         else:
-            print("invalid input")
-
-    def add_city(city):
-        for i in cities.city_list:
-            if city not in cities.city_list:
+            adj[main_city] = [edgecity]
         
-                cities.city_list.append(city)
-                cities.city_graph [city] = []
-            else:
-                print("this city is already added")
-
-    def show_cities():
-        if len(cities.city_list) == 0:
-            print("no cities to show !")
+        if edgecity in adj:
+            adj[edgecity].append(main_city)
         else:
+            adj[edgecity] = [main_city]
 
-            for i in cities.citylist:
-                print(i)
+    def citiesoption(self):
+        answer = input("1. To show cities\n2. Print neighboring cities\n3. Print drivers delivering to city\n")
+        while True:
+           
+        
+            if answer == "1":
+                self.show_cities()
+            elif answer == "2":
+                self.neighbor_cities()
+            elif answer == "3":
+                self.drivers_to_city()
+            elif answer == "4":
+                self.main_instance.welcome()
+            else:
+                print("invalid input")
+                break
+
+    @staticmethod
+    def add_city(city):
+        if city not in cities.city_list:
+            cities.city_list.append(city)
+            cities.city_graph[city] = []
+        else:
+            print("This city is already added")
+
+    def show_cities(self):
+        if not cities.city_list:
+            print("No cities to show!")
+        else:
+            for city in cities.city_list:
+                print(city)
 
     def neighbor_cities(self):
-
-        city = input("enter the name of the city:")
+        city = input("Enter the name of the city: ")
         if city in cities.city_graph:
-            print("the neighbor of {city} is "+ cities.city_graph[city])
+            print(f"The neighbors of {city} are: {cities.city_graph[city]}")
         else:
-            print("{city} is not in the city list.")
+            print(f"{city} is not in the city list.")
 
+    def drivers_to_city(self):
+        city = input("Enter the name of the city: ")
+        visited = set()
+        driver_to_city = self.deliver_to(visited, cities.city_graph, city)
+        if not driver_to_city:
+            print(f"No available drivers to {city}")
+        else:
+            for driver in driver_to_city:
+                print(driver)
 
-    def deliver_to(self,visited,graph,city):
+    @staticmethod
+    def deliver_to(visited, graph, city):
         driver_to_city = []
-
         queue = [city]
 
         while queue:
             current = queue.pop(0)
             if current not in visited:
                 visited.add(current)
-
-                queue.extend(graph[current])
-
+                queue.extend(graph.get(current, []))
                 for driver in menudrivers.driverlist:
-                    if driver["startcity"] == current:
+                    if driver[1] == current:
                         driver_to_city.append(driver)
+        return driver_to_city
 
-        if not driver_to_city:
-            print("No available drivers to this {city}")
-        else:
-            for driver in driver_to_city:
-                print(driver)
+# Initializing cities
+citiess = ["jbeil", "beirut", "akkar", "zahle", "saida"]
+objectt = cities(main)
+for city in citiess:
+    objectt.add_city(city)
 
-cities = ["jbeil","beirut","akkar","zahle","saida"]
-
-
-
-
-    
-
-
-
+cities.addEdge(cities.city_graph, "beirut", "jbeil")
+cities.addEdge(cities.city_graph, "jbeil", "akkar")
+cities.addEdge(cities.city_graph, "saida", "zahle")
+2
 
 if __name__ == "__main__":
     system = main()
     system.welcome()
-
-
-#"""
-
-        
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
